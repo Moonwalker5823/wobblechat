1804ff10f17240bff4cc9d7936d42f085abe8d67
Kelly Yeo <kellyyeo@Kellys-MacBook-Air.local>
2021-08-04 13:53:52 -0400

    questionCard tests

--- client/components/QuestionCard.test.jsx
+++ client/components/QuestionCard.test.jsx
@@ -1,16 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import QuestionCard from './QuestionCard';
 
-it('renders a question card', () => {
+
+it('renders a button card', () => {
     render(<QuestionCard
-                key='1'
-                title='test title'
-                description='test description'
-                creator='test creator'
-                chatURL='test url'
-            />
+        key='1'
+        title='test title'
+        description='test description'
+        creator='test creator'
+        chatURL='test url'
+    />
     );
-    const questionElement = screen.getByLabelText(/answer question/i);
-    expect(questionElement).toBeInTheDocument();
+    const buttonElement = screen.getByText(/answer question/i); // Button itself
+    const titleElement = screen.getByText(/test title/i);
+    const description = screen.getByText(/test description/i);
+
+    screen.debug();
 });
+
+// Check whether the button actually makes a GET request (to connect to the websockets server??)
+
+
