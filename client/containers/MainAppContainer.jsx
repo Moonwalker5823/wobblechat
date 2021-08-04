abab0041b5a8c72349818923563df6fa2953dafc
Harrison Cramer <harrisoncramer@gmail.com>
2021-08-04 12:47:40 -0400

    Rewrote main container as functional component.

--- client/containers/MainAppContainer.jsx
+++ client/containers/MainAppContainer.jsx
@@ -1,77 +1,82 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import QuestionsContainer from "./QuestionsContainer";
 import Chat from "../components/Chat";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-class MainAppContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      questionBool: true,
-      fetchedData: false,
-      questions: [],
-    };
-  }
+const Main = () => {
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [questions, setQuestions] = useState([]);
+  const history = useHistory();
 
-  componentDidMount() {
-    fetch("/api/questions")
-      .then((res) => res.json())
-      .then((questionData) => {
-        if (!Array.isArray(questionData)) questionData = [];
-        this.setState({ fetchedData: true, questions: questionData });
+  const handleLogout = () => {
+    fetch("/api/users/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((_) => {
+        history.push("/");
       })
-      .catch((err) =>
-        console.log(
-          "MainAppContainer.componentDidMount has error when making fetch request for questionsData: ERROR: ",
-          err
-        )
-      );
-  }
+      .catch((_err) => {
+        alert("Could not log you out!");
+      });
+  };
 
-  // fix Warning: Can't perform a React state update on an unmounted component
-  componentWillUnmount() {
-    this.setState = () => {};
-  }
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("/api/questions")
+      .then((response) => response.json())
+      .then((data) => {
+        setIsLoading(false);
+        setQuestions(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsError(true);
+      });
+  }, []);
 
-  render() {
-    if (!this.state.fetchedData)
-      return (
-        <div>
-          <h2>Loading data, please wait...</h2>
-        </div>
-      );
-
-    const { questions } = this.state;
-    if (!questions) return null;
-    if (!questions.length) {
-      return <div>Sorry, no questions to display</div>;
-    }
-
+  if (isLoading)
     return (
-      <>
-        <div className="top-menu-container">
-          <Link to={"/"} className="brandname">
-            Wobblechat
-          </Link>
-          <div>
-            <Link to={"/create"}>
-              <Button variant="primary">Ask a question</Button>
-            </Link>
-            <Button variant="secondary">Sign out</Button>
-          </div>
-        </div>
-
-        <Card className="chat-style">
-          <Chat />
-        </Card>
-        <br />
-        <QuestionsContainer questions={this.state.questions} />
-      </>
+      <div>
+        <h2>Loading data, please wait...</h2>
+      </div>
     );
-  }
-}
 
-export default MainAppContainer;
+  if (!questions.length) {
+    return <div>Sorry, no questions to display</div>;
+  }
+
+  if (isError) {
+    return <div>An error has occured fetching the questions!</div>;
+  }
+
+  return (
+    <>
+      <div className="top-menu-container">
+        <Link to={"/"} className="brandname">
+          Wobblechat
+        </Link>
+        <div>
+          <Link to={"/create"}>
+            <Button variant="primary">Ask a question</Button>
+          </Link>
+          <Button variant="secondary" onClick={handleLogout}>
+            Log out
+          </Button>
+        </div>
+      </div>
+      <Card className="chat-style">
+        <Chat />
+      </Card>
+      <br />
+      <QuestionsContainer questions={questions} />
+    </>
+  );
+};
+
+export default Main;
